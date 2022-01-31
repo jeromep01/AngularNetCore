93f460db519c50855927a097e105f27e090a7dfc
jperciot <jperciot@oogarden.com>
2022-01-31 22:30:35 +0100

    health check (ts + css +html)

--- AngularTestProjects/HealthCheckNetCore3/ClientApp/src/app/app.module.ts
+++ AngularTestProjects/HealthCheckNetCore3/ClientApp/src/app/app.module.ts
@@ -7,22 +7,25 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
+import { HealthCheckComponent } from './health-check/health-check.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavMenuComponent,
-    HomeComponent
-  ],
-  imports: [
-    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
-    HttpClientModule,
-    FormsModule,
-    RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' }
-    ])
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        NavMenuComponent,
+        HomeComponent,
+        HealthCheckComponent
+    ],
+    imports: [
+        BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+        HttpClientModule,
+        FormsModule,
+        RouterModule.forRoot([
+            { path: '', component: HomeComponent, pathMatch: 'full' },
+            { path: 'health-check', component: HealthCheckComponent }
+        ])
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ AngularTestProjects/HealthCheckNetCore3/ClientApp/src/app/health-check/health-check.component.ts
@@ -0,0 +1,35 @@
+﻿import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+    selector: 'app-health-check',
+    templateUrl: './health-check.component.html',
+    styleUrls: ['./health-check.component.css']
+})
+export class HealthCheckComponent {
+    public result: Result;
+
+    constructor(
+        private http: HttpClient,
+        @Inject('BASE_URL') private baseUrl: string) {
+
+    }
+
+    ngOnInit() {
+        this.http.get<Result>(this.baseUrl + 'hc').subscribe(result => {
+            this.result = result;
+        }, error => console.error(error));
+    }
+}
+
+interface Result {
+    checks: Check[];
+    totalStatus: number;
+    totalResponseTime: number;
+}
+
+interface Check {
+    name: String,
+    status: String;
+    responseTime: number;
+}
